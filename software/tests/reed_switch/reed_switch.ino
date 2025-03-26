7494c32ff7eba3f30e384b160c005831abf46b5a
nihaltp <nihaltpnki@gmail.com>
2025-03-27 02:52:32 +0530

    Refactor reed switch code for improved sensor reading and multiplexing logic

--- software/tests/reed_switch/reed_switch.ino
+++ software/tests/reed_switch/reed_switch.ino
@@ -1,72 +1,90 @@
 /* 
+ * Version: 2.0
+ *
  * Code for Arduino
  *
  * Reed switch code that goes through all 64 sensors placed in a 8x8 matrix
  * Using 4 x 74HC4067 multiplexers TODO: Change the name
  * one for two rows since they have 16 input pins
- * the other end of each row provides GND and one takes signal
- * the signal pin of multiplexers are connected to A0 - A3
+ * one end of the sensor is connected to GND and the other end gives signal
+ * the SIG (signal) pin of multiplexers are shorted and connected to A0
+ * the enable pins of multiplexers are connected to digital pins 2, 3, 4, 5
  *
  * Made by: NIHAL T P
  * GitHub: https://github.com/nihaltp
  * LinkedIn: https://www.linkedin.com/in/nihal-tp
  *
- *
  * Circuit:
  *
- *  V_in
- *   |
- *   R1 (2kΩ)
- *   |
- *  -----> V_out (to ESP8266 A0)
- *   |
- *   R2 (1kΩ)
- *   |
  *  GND
+ *   |
+ * Sensor
+ *   |
+ *  -----> Out (to Arduino)
+ *   |
+ *  1kΩ (internal pull-up)
+ *   |
+ *   5V
  *
- */
+*/
 
-//MUX1 control pins    // TODO: Adjust Pins
-#define ROW1 5
-#define ROW2 6
-#define ROW3 7
-#define ROW4 8
+#include <Arduino.h>
 
-#define IN1 A0
-#define IN2 A1
-#define IN3 A2
-#define IN4 A3
+// TODO: Adjust Pins
+//MUX control pins
+#define ROW1 8   // S0
+#define ROW2 9   // S1
+#define ROW3 10  // S2
+#define ROW4 11  // S3
+
+// MUX enable pins
+#define EN1 2    // MUX1
+#define EN2 3    // MUX2
+#define EN3 4    // MUX3
+#define EN4 5    // MUX4
+
+#define IN A0    // Signal
 
 // Array to store values from each sensor
-int sensorValues[64];
+bool sensorValues[64];
+
+void readMux(int num);
 
 // MARK: setup
 void setup() {
-  Serial.begin(9600);
-
+  Serial.begin(115200);
+  
   pinMode(ROW1, OUTPUT);
   pinMode(ROW2, OUTPUT);
   pinMode(ROW3, OUTPUT);
   pinMode(ROW4, OUTPUT);
-
-  pinMode(IN1, INPUT);
-  pinMode(IN2, INPUT);
-  pinMode(IN3, INPUT);
-  pinMode(IN4, INPUT);
-
-  digitalWrite(ROW1, LOW);
-  digitalWrite(ROW2, LOW);
-  digitalWrite(ROW3, LOW);
-  digitalWrite(ROW4, LOW);
+  
+  pinMode(IN, INPUT_PULLUP);
+  
+  pinMode(EN1, OUTPUT);
+  pinMode(EN2, OUTPUT);
+  pinMode(EN3, OUTPUT);
+  pinMode(EN4, OUTPUT);
+  
+  // Enable of multiplexer is active low
+  digitalWrite(EN1, HIGH);
+  digitalWrite(EN2, HIGH);
+  digitalWrite(EN3, HIGH);
+  digitalWrite(EN4, HIGH);
 }
 
 // MARK: loop
 void loop() {
   //Loop through and read all 64 values
-  for (int i = 0; i < 16; i++) {
+  Serial.print("<");
+  for (int i = 0; i < 64; i++) {
     readMux(i);
+    Serial.print(sensorValues[i]);
+    if (i < 63) {
+      Serial.print(",");
+    }
   }
-  printValues();
+  Serial.println(">");
 }
 
 // MARK: readMux
@@ -76,23 +94,14 @@ void readMux(int num) {
   digitalWrite(ROW2, bitRead(num, 1));
   digitalWrite(ROW3, bitRead(num, 2));
   digitalWrite(ROW4, bitRead(num, 3));
-
-  // Control pins of each sensor is connected
-  // so we can read 4 values at the same time
-  // mux controls 16 sensors so diffrence is 16
-
-  sensorValues[num] = digitalRead(IN1);
-  sensorValues[num + 16] = digitalRead(IN2);
-  sensorValues[num + 32] = digitalRead(IN3);
-  sensorValues[num + 48] = digitalRead(IN4);
-}
-
-// MARK: printValues
-void printValues() {
-  for (int i = 0; i < 64; i++) {
-    if (i % 8 == 0) Serial.println();
-    Serial.print(sensorValues[i]);
-    Serial.print(" ");
-  }
-  Serial.println();
+  
+  // Enable the corresponding multiplexer based on the sensor number
+  digitalWrite(EN1, (num < 16)              ? LOW : HIGH);
+  digitalWrite(EN2, (num >= 16 && num < 32) ? LOW : HIGH);
+  digitalWrite(EN3, (num >= 32 && num < 48) ? LOW : HIGH);
+  digitalWrite(EN4, (num >= 48)             ? LOW : HIGH);
+  
+  delay(50);
+  
+  sensorValues[num] = digitalRead(IN);
 }
